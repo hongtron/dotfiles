dba0ca3bbdbf9b683dc903ad80d4e3d4c1cd2b2b
ali <github@alihong.net>
2018-12-28 12:17:31 -0600

    add vpc install script

---
+++ vpc_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -o pipefail
+
+# install docker
+if [[ -z "$(which docker)" ]]; then
+  curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
+fi
+
+# install docker-compose
+if [[ -z "$(which docker-compose)" ]]; then
+  curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
+fi
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cp $DIR/bin/devenv /usr/local/bin
+cp $DIR/docker-compose.yml /usr/local/etc/docker-compose.yml

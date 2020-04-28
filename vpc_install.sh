febfa7e325f8f4d534c79e80febd5e3177ba7f61
ali hong <hongtron@users.noreply.github.com>
2020-04-27 19:20:47 -0500

    update docker-compose in vpc install

--- vpc_install.sh
+++ vpc_install.sh
@@ -9,7 +9,7 @@ fi
 
 # install docker-compose
 if [[ -z "$(which docker-compose)" ]]; then
-  curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
 fi
 
